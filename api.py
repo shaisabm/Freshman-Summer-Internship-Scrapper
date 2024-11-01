ea29577c7b55a5df990d55735084de3086dcc2ed
Shaisab Mistry <shaisabmistry@gmail.com>
2024-11-01 11:54:44 -0400

    Gemini api setup

---
+++ api.py
@@ -0,0 +1,29 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+from datetime import date
+
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+# Create the model
+
+def gemini(job_description):
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash-8b",
+        system_instruction= "Today is " + str(date.today()) + "."
+                            "Return the json of the job description with the following keys:"
+                            "if no mention of freshman, sophomore, junior or senior then hiring_freshman: true "
+                             "Also if graduations year includes 2028 then hiring_freshman: true. "                                 
+                            "list({"
+                            "hiring_freshman: boolean, "
+                            "Posted_days_ago: number,"
+                            "})"
+
+        )
+
+    response = model.generate_content(f"Job Description: {job_description}")
+
+    return response.text
+
